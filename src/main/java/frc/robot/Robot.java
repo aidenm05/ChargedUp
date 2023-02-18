eea100fb36ef259a0f661f4e1ef5873d63ff8332
gsesny <greg_sesny@collegiate-va.org>
2023-02-18 10:11:06 -0500

    auto path changes

--- src/main/java/frc/robot/Robot.java
+++ src/main/java/frc/robot/Robot.java
@@ -4,6 +4,8 @@
 
 package frc.robot;
 
+import com.pathplanner.lib.server.PathPlannerServer;
+
 import edu.wpi.first.wpilibj.TimedRobot;
 import edu.wpi.first.wpilibj2.command.Command;
 import edu.wpi.first.wpilibj2.command.CommandScheduler;
@@ -35,6 +37,7 @@ public class Robot extends TimedRobot {
   public void robotInit() {
     ctreConfigs = new CTREConfigs();
     Constants.confirmMantisConstants();
+    PathPlannerServer.startServer(5811); // 5811 = port number. adjust this according to your needs
 
     // Instantiate our RobotContainer.  This will perform all our button bindings, and put our
     // autonomous chooser on the dashboard.
